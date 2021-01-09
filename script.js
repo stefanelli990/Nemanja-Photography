19e426d7c8fbc272baf14ed92609de3b3a17e2e2
stefanelli990 <stefan.mohenski@gmail.com>
2021-01-09 20:46:44 +0100

    last change

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
+//hamburger slide
 const toggle = document.querySelector('.navigation__toggle');
 const hamburgerMenu = document.querySelector('.hamburger');
 const body = document.querySelector('body');
 const landingPage = document.querySelector('.landing-page');
-
+const navLinks = document.querySelectorAll('.nav-link');
 
 toggle.addEventListener('click', () => {
     body.classList.toggle('body-scroll');
     toggle.classList.toggle('change');
     landingPage.classList.toggle('landing-page-active');
-    hamburgerMenu.classList.toggle('hamburger-active')
+    hamburgerMenu.classList.toggle('hamburger-active');
+});
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        body.classList.remove('body-scroll');
+        toggle.classList.remove('change');
+        landingPage.classList.remove('landing-page-active');
+        hamburgerMenu.classList.remove('hamburger-active');
+    });
+});
+
+
+
+//gallery
+const images = document.querySelectorAll('.images');
+const modal = document.querySelector('.modal');
+const modalImg = document.querySelector('.modal-image');
+
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        modal.classList.add('modal-active');
+
+
+        //dynamicaly change image
+        const imgSrc = image.getAttribute('src');
+        modalImg.src = `${imgSrc}`;
+    });
+});
+
+// close image 
+modal.addEventListener('click', (e) => {
+    if(e.target.classList.contains('modal')) {
+        modal.classList.remove('modal-active');
+    }
 });
