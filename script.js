05b079d80f1f31a1f23a7f22cedd54585e0b68f1
stefanelli990 <stefan.mohenski@gmail.com>
2021-04-13 02:35:05 +0200

    last change

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
 const toggle = document.querySelector('.navigation__toggle');
+const close = document.querySelector('.hamburger__close');
 const hamburgerMenu = document.querySelector('.hamburger');
 const body = document.querySelector('body');
 const landingPage = document.querySelector('.landing-page');
 const navLinks = document.querySelectorAll('.nav-link');
 const navigation = document.querySelector('.navigation');
+const logo = document.querySelector('.navigation__logo');
 
 
-//hamburger menu slide
+//hamburger menu open
 toggle.addEventListener('click', () => {
-    body.classList.toggle('body-scroll');
-    toggle.classList.toggle('change');
-    landingPage.classList.toggle('landing-page-active');
-    hamburgerMenu.classList.toggle('hamburger-active');
-    
+    body.classList.add('body-scroll');
+    hamburgerMenu.classList.add('hamburger-active');
 });
+
+//hamburger close
+close.addEventListener('click', () => {
+    body.classList.remove('body-scroll');
+    hamburgerMenu.classList.remove('hamburger-active');
+})
 
 //close hamburger when clicks on hamburger navlinks
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
         body.classList.remove('body-scroll');
-        toggle.classList.remove('change');
-        landingPage.classList.remove('landing-page-active');
+      
         hamburgerMenu.classList.remove('hamburger-active');
     });
 });
 
 //prevent hamburger active when scrolls down at bigger window
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 0) {
-        body.classList.remove('body-scroll');
-        toggle.classList.remove('change');
-        landingPage.classList.remove('landing-page-active');
-        hamburgerMenu.classList.remove('hamburger-active');
-    } 
-});
+// window.addEventListener('scroll', () => {
+//     if(window.pageYOffset > 0) {
+//         body.classList.remove('body-scroll');
+//         toggle.classList.remove('change');
+//         landingPage.classList.remove('landing-page-active');
+//         hamburgerMenu.classList.remove('hamburger-active');
+//     } 
+// });
 
 // navigation animation 
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 100) {
+    if(window.pageYOffset > 500) {
         navigation.classList.add('navigation-animation');
+        logo.firstElementChild.setAttribute('src', 'images/logo-black.svg');
+        
 
     } else {
         navigation.classList.remove('navigation-animation');
+        logo.firstElementChild.setAttribute('src', 'images/logo-white.svg');
     }
 });
 
