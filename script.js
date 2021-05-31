ec9b53342f73a3ae58fac6bb4a51264071dca276
stefanelli990 <stefan.mohenski@gmail.com>
2021-05-31 17:50:58 +0200

    last change

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-const toggle = document.querySelector('.navigation__toggle');
-const close = document.querySelector('.hamburger__close');
-const hamburgerMenu = document.querySelector('.hamburger');
-const body = document.querySelector('body');
-const navLinks = document.querySelectorAll('.nav-link');
-const navigation = document.querySelector('.navigation');
-const logo = document.querySelector('.navigation__logo');
+const toggle = document.querySelector('.toggle');
+const sideNav = document.querySelector('.sidenav');
 
 
-//hamburger menu open
 toggle.addEventListener('click', () => {
-    body.classList.add('body-scroll');
-    hamburgerMenu.classList.add('active');
-});
-
-//hamburger close
-close.addEventListener('click', () => {
-    body.classList.remove('body-scroll');
-    hamburgerMenu.classList.remove('active');
-})
-
-//close hamburger when clicks on hamburger navlinks
-navLinks.forEach(navLink => {
-    navLink.addEventListener('click', () => {
-        body.classList.remove('body-scroll');
-        hamburgerMenu.classList.remove('active');
-    });
-});
-
-
-//prevent hamburger active when scrolls down at bigger window
-// window.addEventListener('scroll', () => {
-//     if(window.pageYOffset > 0) {
-//         body.classList.remove('body-scroll');
-//         toggle.classList.remove('change');
-//         landingPage.classList.remove('landing-page-active');
-//         hamburgerMenu.classList.remove('hamburger-active');
-//     } 
-// });
-
-
-// navigation animation 
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 500) {
-        navigation.classList.add('animation');
-    } else {
-        navigation.classList.remove('animation');
-    }
+    sideNav.classList.toggle('active');
+   
 });
 
 //glightbox
@@ -53,6 +12,7 @@ const lightbox = GLightbox({
     zoomable: false,
     openEffect: 'fade',
     closeEffect: 'fade'
+    
 });
 
 
