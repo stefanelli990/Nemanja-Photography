7d5f44e800b025482db198dc35b606cf37a74d36
stefanelli990 <stefan.mohenski@gmail.com>
2021-06-12 01:43:22 +0200

    last change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const toggle = document.querySelector('.toggle');
 const sideNav = document.querySelector('.sidenav');
+const loader = document.querySelector('.loader');
 
 
 toggle.addEventListener('click', () => {
@@ -15,4 +16,9 @@ const lightbox = GLightbox({
     
 });
 
+
+setTimeout(() => {
+    loader.classList.add('active');
+    body.style.overflowY = 'visible';
+}, 3000);
 
